5d2dcec7fe4dacf0acb59607caf00a6fd088572f
dobean99 <kimdo060599@gmail.com>
2023-11-08 16:35:23 +0700

    add custom spin loading

--- lib/features/loading_screen/loading_screen.dart
+++ lib/features/loading_screen/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
+import 'package:balloon_in_the_sky/core/constants/app_constants.dart';
+import 'package:balloon_in_the_sky/features/loading_screen/custom_spin.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import '../../core/constants/app_colors.dart';
 import '../shared/shared.dart';
 
@@ -12,19 +13,29 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  late Timer _timer;
+
   void navigationPage() {
     Navigator.of(context).pushReplacementNamed('/MenuScreen');
   }
 
-  startTime() async {
-    var duration = const Duration(seconds: 10);
-    return Timer(duration, navigationPage);
-  }
+  // startTime() async {
+  //   var duration = const Duration(seconds: 10);
+  //   return Timer(duration, navigationPage);
+  // }
 
   @override
   void initState() {
     super.initState();
-    startTime();
+    _timer = Timer(const Duration(seconds: 1), () {
+      navigationPage();
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
   }
 
   @override
@@ -32,13 +43,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
     return BaseLayout(
       bottomButton: Padding(
         padding: const EdgeInsets.only(bottom: 50.0),
-        child: SpinKitFadingCircle(
+        child: CustomSpin(
           itemBuilder: (BuildContext context, int index) {
             return DecoratedBox(
               decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
                 border: Border.all(
                   color: AppColors.whiteColor,
-                  width: 1.0,
+                  width: AppConstants.borderWidth,
                 ),
                 borderRadius: BorderRadius.circular(10),
                 gradient: AppColors.blackToGrey,
@@ -46,8 +58,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
               ),
             );
           },
-          size: 200.0,
-          duration: const Duration(seconds: 1),
+          size: 100.0,
+          // duration: const Duration(seconds: 1),
         ),
       ),
     );
